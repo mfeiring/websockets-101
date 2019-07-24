320a048d811b030cf0072c738c756699b1cddba7
Mira Thoen Feiring <mirafeiring@gmail.com>
2019-07-24 12:33:25 +0200

    bare omrokkerte litt på import av filer

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import io from 'socket.io-client';
+
 import Participants from './components/Participants';
 import Chat from './components/Chat';
-
-import './index.css';
 import Game from './components/Game';
+import './index.css';
 
 const socket = io('http://localhost:3000');
 
